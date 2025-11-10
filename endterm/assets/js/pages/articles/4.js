3accdcf8cfaceddd24dd09520e205378d9875648
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2025-11-11 03:29:06 +0500

    CRUD

---
+++ endterm/assets/js/pages/articles/4.js
@@ -0,0 +1,263 @@
+const table = [
+	{
+		id: 1,
+		first_name: "Mark",
+		last_name: "Zukerberg",
+	},
+	{
+		id: 2,
+		first_name: "John",
+		last_name: "Doe",
+	},
+];
+
+let IDs = table.length;
+let CURRENT_ID = null;
+
+function sanitize(str) {
+	return str.toString().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+
+function renderTable(newCount) {
+	newCount = newCount ?? 0;
+
+	const tableElement = $("#table");
+	if (tableElement == null) {
+		return;
+	}
+
+	const tableBody = tableElement.find("tbody");
+	tableBody.empty();
+
+	for (const entry of table) {
+		const row = $(`
+			<tr id="entry-${sanitize(entry.id)}" class="entry">
+				<th id="id" scope="row">${sanitize(entry.id)}</th>
+				<td id="first_name">${sanitize(entry.first_name)}</td>
+				<td id="last_name">${sanitize(entry.last_name)}</td>
+				<td class="align-bottom">
+					<button type="button" class="btn btn-primary px-1 py-0" data-bs-toggle="modal" data-bs-target="#editModal">
+						Edit
+					</button>
+					<button type="button" class="btn btn-danger px-1 py-0" data-bs-toggle="modal" data-bs-target="#deleteModal">
+						Delete
+					</button>
+				</td>
+			</tr>
+		`);
+
+		if (IDs - newCount < entry.id) {
+			row.hide().fadeIn();
+		}
+
+		row.find(".btn-primary").on("click", () => {
+			const editForm = document.querySelector("#editEntryForm");
+			const first_name = editForm.querySelector("#first_name");
+			const last_name = editForm.querySelector("#last_name");
+
+			first_name.value = entry.first_name;
+			last_name.value = entry.last_name;
+
+			CURRENT_ID = entry.id;
+		});
+
+		row.find(".btn-danger").on("click", () => {
+			CURRENT_ID = entry.id;
+		});
+
+		tableBody.append(row);
+	}
+
+	const addNewRow = $(`
+		<tr id="add">
+			<th scope="row">
+				<button type="button" class="btn btn-success px-1 py-0" data-bs-toggle="modal" data-bs-target="#addNewModal">
+					Add new
+				</button>
+			</th>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	`);
+
+	tableBody.append(addNewRow);
+}
+
+function OnDOMContentLoaded() {
+	const addNewForm = document.querySelector("#addEntryForm");
+
+	if (addNewForm != null) {
+		const modal = new bootstrap.Modal(document.querySelector("#addNewModal"), {});
+		const first_name = addNewForm.querySelector("#first_name");
+		const last_name = addNewForm.querySelector("#last_name");
+
+		addNewForm.addEventListener("submit", (event) => {
+			addNewForm.classList.add("was-validated");
+
+			if (!addNewForm.checkValidity()) {
+				event.preventDefault();
+				event.stopPropagation();
+				return;
+			}
+
+			table.push({
+				id: ++IDs,
+				first_name: sanitize(first_name.value),
+				last_name: sanitize(last_name.value),
+			});
+
+			modal.hide();
+
+			renderTable(1);
+
+			addNewForm.reset();
+			addNewForm.classList.remove("was-validated");
+
+			event.preventDefault();
+			event.stopPropagation();
+		});
+	}
+
+	const editForm = document.querySelector("#editEntryForm");
+
+	if (editForm != null) {
+		const modal = new bootstrap.Modal(document.querySelector("#editModal"), {});
+		const first_name = editForm.querySelector("#first_name");
+		const last_name = editForm.querySelector("#last_name");
+
+		editForm.addEventListener("submit", (event) => {
+			if (CURRENT_ID == null) {
+				event.preventDefault();
+				event.stopPropagation();
+				return;
+			}
+
+			editForm.classList.add("was-validated");
+
+			if (!editForm.checkValidity()) {
+				event.preventDefault();
+				event.stopPropagation();
+				return;
+			}
+
+			for (const entry of table) {
+				if (entry.id == CURRENT_ID) {
+					entry.first_name = sanitize(first_name.value);
+					entry.last_name = sanitize(last_name.value);
+					break;
+				}
+			}
+
+			modal.hide();
+
+			renderTable();
+
+			CURRENT_ID = null;
+
+			editForm.reset();
+			editForm.classList.remove("was-validated");
+
+			event.preventDefault();
+			event.stopPropagation();
+		});
+	}
+
+	const deleteModal = document.querySelector("#deleteModal");
+
+	if (deleteModal != null) {
+		const modal = new bootstrap.Modal(deleteModal, {});
+
+		deleteModal.addEventListener("click", () => {
+			if (CURRENT_ID == null) {
+				return;
+			}
+
+			const index = table.indexOf(table.find(entry => entry.id == CURRENT_ID));
+
+			if (index === -1) {
+				return;
+			}
+
+			table.pop(index);
+
+			modal.hide();
+
+			const entry = $(`#table tbody #entry-${sanitize(CURRENT_ID)}`);
+
+			CURRENT_ID = null;
+
+			if (entry != null) {
+				entry.fadeOut("fast", renderTable);
+			}
+		});
+	}
+
+	renderTable();
+
+	const searchbar = document.querySelector("#search-bar");
+
+	if (searchbar != null) {
+		const tableBody = document.querySelector("#table tbody");
+
+		searchbar.addEventListener("input", () => {
+			if (searchbar.value.length <= 0) {
+				if (tableBody.classList.contains("searching")) {
+					tableBody.classList.remove("searching");
+				}
+
+				for (const row of tableBody.querySelectorAll(".entry")) {
+					if (row.classList.contains("search-match")) {
+						row.classList.remove("search-match");
+					}
+
+					if (row.classList.contains("search-match-best")) {
+						row.classList.remove("search-match-best");
+					}
+				}
+			} else {
+				const searchText = searchbar.value.toLowerCase();
+
+				if (!tableBody.classList.contains("searching")) {
+					tableBody.classList.add("searching");
+				}
+
+				for (const row of tableBody.querySelectorAll(".entry")) {
+					const rowId = row.querySelector("#id");
+					const rowFirstName = row.querySelector("#first_name");
+					const rowLastName = row.querySelector("#last_name");
+					const sameRowId = rowId != null ? rowId.textContent == searchText : false;
+					const firstNameSimilarity = rowFirstName != null ? partial_ratio_similarity(searchText, rowFirstName.textContent.toLowerCase()) : 0;
+					const lastNameSimilarity = rowLastName != null ? partial_ratio_similarity(searchText, rowLastName.textContent.toLowerCase()) : 0;
+
+					const matching = sameRowId || firstNameSimilarity > 0.5 || lastNameSimilarity > 0.5;
+					const bestMatching = matching && (sameRowId || firstNameSimilarity >= 0.8 || lastNameSimilarity >= 0.8);
+
+					if (matching) {
+						if (!row.classList.contains("search-match")) {
+							row.classList.add("search-match");
+						}
+
+						if (bestMatching) {
+							if (!row.classList.contains("search-match-best")) {
+								row.classList.add("search-match-best");
+							}
+						} else if (row.classList.contains("search-match-best")) {
+							row.classList.remove("search-match-best");
+						}
+					} else {
+						if (row.classList.contains("search-match")) {
+							row.classList.remove("search-match");
+						}
+
+						if (row.classList.contains("search-match-best")) {
+							row.classList.remove("search-match-best");
+						}
+					}
+				}
+			}
+		});
+	}
+}
+
+document.addEventListener("DOMContentLoaded", OnDOMContentLoaded);
--- endterm/assets/js/script.js
+++ endterm/assets/js/script.js
@@ -1,3 +1,83 @@
+function similarity(s1, s2) {
+	return 1.0 - normalized_levenshtein(s1, s2);
+}
+
+function normalized_levenshtein(s1, s2) {
+	const levenshtein = new Levenshtein(s1, s2);
+
+	return levenshtein.distance / Math.max(s1.length, s2.length);
+}
+
+function partial_ratio_similarity(s1, s2) {
+	/*
+		Algorithm taken from https://github.com/rapidfuzz/RapidFuzz/blob/main/src/rapidfuzz/fuzz_py.py#L116-L179
+	*/
+	const len1 = s1.length;
+	const len2 = s2.length;
+
+	if (len1 > len2) {
+		return partial_ratio_similarity(s2, s1);
+	}
+
+	let score = 0.0;
+
+	const s1_char_set = new Set();
+
+	for (const c of s1) {
+		s1_char_set.add(c);
+	}
+
+	for (let i=1; i<len1; i++) {
+		const substr_last = s2.charAt(i-1);
+		if (!s1_char_set.has(substr_last)) {
+			continue;
+		}
+
+		const ls_ratio = similarity(s1, s2.substring(0, i));
+
+		if (ls_ratio > score) {
+			score = ls_ratio;
+			if (score >= 100.0) {
+				return score;
+			}
+		}
+	}
+
+	for (let i=0; i<len2-len1; i++) {
+		const substr_last = s2.charAt(i + len1 - 1);
+		if (!s1_char_set.has(substr_last)) {
+			continue;
+		}
+
+		const ls_ratio = similarity(s1, s2.substring(i, i + len1));
+
+		if (ls_ratio > score) {
+			score = ls_ratio;
+			if (score >= 100.0) {
+				return score;
+			}
+		}
+	}
+
+	for (let i=len2-len1; i<len2; i++) {
+		const substr_first = s2.charAt(i);
+		if (!s1_char_set.has(substr_first)) {
+			continue;
+		}
+
+		const ls_ratio = similarity(s1, s2.substring(i));
+
+		if (ls_ratio > score) {
+			score = ls_ratio;
+			if (score >= 100.0) {
+				return score;
+			}
+		}
+	}
+
+	return score;
+}
+
 async function loadViews() {
 	for (const element of document.querySelectorAll("[data-import]")) {
 		const url = `/views/${element.dataset.import}.html`;
@@ -23,7 +103,7 @@ async function loadViews() {
 
 function loadBootstrapForms() {
 	for (const form of document.querySelectorAll("form.needs-validation")) {
-		form.addEventListener("submit", event => {
+		form.addEventListener("submit", (event) => {
 			if (!form.checkValidity()) {
 				event.preventDefault();
 				event.stopPropagation();
