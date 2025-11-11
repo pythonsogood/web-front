d7b4f3374f5fab09e9c3645dd873b78d04889255
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2025-11-11 23:36:02 +0500

    endterm

---
+++ endterm/README.md
@@ -0,0 +1,31 @@
+# Endterm projct. Blog
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/license/mit/)
+
+## Pages
+
+- [x] [Home page](https://pythonsogood.github.io/index.html)
+- [x] [Articles page](https://pythonsogood.github.io/articles.html)
+- [x] [About page](https://pythonsogood.github.io/about.html)
+- [x] [Contacts page](https://pythonsogood.github.io/contacts.html)
+- [x] [Resources page](https://pythonsogood.github.io/resources.html)
+
+## Features
+
+- [x] Bootstrap navigation bar
+- [x] Footer with Bootstrap Grid
+- [x] Table at [Resources page](https://pythonsogood.github.io/resources.html)
+- [x] Animated form with Bootstrap validation and feedback at [Contacts page](https://pythonsogood.github.io/contacts.html)
+- [x] Slogan with `span` and `transition` animation at [Home page](https://pythonsogood.github.io/index.html)
+- [x] `position: sticky` for header
+- [x] `:nth-child(even)` at [Contacts page](https://pythonsogood.github.io/contacts.html)
+- [x] `:hover` at [Contacts page](https://pythonsogood.github.io/contacts.html)
+- [x] 3 color variables at `:root`
+- [x] Google Fonts: `Lexend Deca` and `Lato`
+- [x] Lazy image loading
+- [x] Responsive design with desktop-first approach
+- [x] Real-time search bar at [Articles page](https://pythonsogood.github.io/articles.html) with using Levenshtein distance algorithm
+- [x] Bootstrap Modal at [Contacts page](https://pythonsogood.github.io/contacts.html)
+- [x] Dynamic CRUD Table at [4'th article page](https://pythonsogood.github.io/articles/4.html)
+- [x] Image Gallery with Lightbox at [5'th article page](https://pythonsogood.github.io/articles/5.html)
+- [x] Dark Mode switcher at header
--- endterm/assets/js/pages/articles/5.js
+++ endterm/assets/js/pages/articles/5.js
@@ -7,12 +7,19 @@ async function OnDOMContentLoaded() {
 	const galleryElement = document.querySelector("#gallery");
 
 	if (galleryElement != null) {
-		for (const element of gallery) {
+		const main = document.querySelector("main");
+
+		for (let i=0; i<gallery.length; i++) {
+			const element = gallery[i];
+
 			const col = document.createElement("div");
 			col.classList.add("col");
 
 			const card = document.createElement("div");
 			card.classList.add("card");
+			card.dataset.categories = element["categories"].join(",");
+			card.dataset.bsToggle = "modal";
+			card.dataset.bsTarget = `#modal-lightbox-${i}`;
 
 			const img = document.createElement("img");
 			img.classList.add("img-fluid", "rounded-3");
@@ -23,6 +30,67 @@ async function OnDOMContentLoaded() {
 			col.appendChild(card);
 
 			galleryElement.appendChild(col);
+
+			const modalElement = document.createElement("div");
+			modalElement.classList.add("modal", "fade");
+			modalElement.id = `modal-lightbox-${i}`;
+			modalElement.ariaHidden = "true";
+			modalElement.tabIndex = "-1";
+
+			const modalDialog = document.createElement("div");
+			modalDialog.classList.add("modal-dialog", "modal-dialog-centered");
+
+			const modalContent = document.createElement("div");
+			modalContent.classList.add("modal-content");
+
+			const modalHeader = document.createElement("div");
+			modalHeader.classList.add("modal-header");
+
+			const modalClose = document.createElement("button");
+			modalClose.classList.add("btn-close");
+			modalClose.dataset.bsDismiss = "modal";
+			modalClose.ariaLabel = "Close";
+
+			const modalBody = document.createElement("div");
+			modalBody.classList.add("modal-body");
+
+			const modalImage = document.createElement("img");
+			modalImage.classList.add("img-fluid", "rounded-3");
+			modalImage.src = element["image"];
+			modalImage.loading = "lazy";
+
+			const modalFooter = document.createElement("div");
+			modalFooter.classList.add("modal-footer");
+
+			if (i > 0) {
+				const modalPrevButton = document.createElement("button");
+				modalPrevButton.classList.add("btn", "btn-primary");
+				modalPrevButton.dataset.bsTarget = `#modal-lightbox-${i-1}`;
+				modalPrevButton.dataset.bsToggle = "modal";
+				modalPrevButton.innerText = "<"
+
+				modalFooter.classList.add("justify-content-between");
+				modalFooter.appendChild(modalPrevButton);
+			}
+
+			if (i < gallery.length - 1) {
+				const modalNextButton = document.createElement("button");
+				modalNextButton.classList.add("btn", "btn-primary");
+				modalNextButton.dataset.bsTarget = `#modal-lightbox-${i+1}`;
+				modalNextButton.dataset.bsToggle = "modal";
+				modalNextButton.innerText = ">";
+
+				modalFooter.appendChild(modalNextButton);
+			}
+
+			modalBody.appendChild(modalImage);
+			modalHeader.appendChild(modalClose);
+			modalContent.appendChild(modalHeader);
+			modalContent.appendChild(modalBody);
+			modalContent.appendChild(modalFooter);
+			modalDialog.appendChild(modalContent);
+			modalElement.appendChild(modalDialog);
+			main.appendChild(modalElement);
 		}
 
 		const filterElement = document.querySelector("#filter");
@@ -30,7 +98,16 @@ async function OnDOMContentLoaded() {
 		if (filterElement != null) {
 			for (const input of filterElement.querySelectorAll("input[name=\"gallery-filter\"]")) {
 				input.addEventListener("change", () => {
-					console.log(input.checked);
+					if (input.checked) {
+						$("#gallery .card").each(function() {
+							const card = $(this);
+							if (input.id == "all" || card.get(0).dataset.categories.split(",").includes(input.id)) {
+								card.parent().fadeIn(350);
+							} else {
+								card.parent().fadeOut(350);
+							}
+						});
+					}
 				});
 			}
 		}
